a5648ffd0600cde1bfd2a9ad376b9c3dd644568e
EC2 Default User <ec2-user@ip-172-31-27-138.ap-southeast-1.compute.internal>
2020-02-27 02:23:58 +0000

    modify schedule

--- crawler/schedule.js
+++ crawler/schedule.js
@@ -3,7 +3,7 @@ var schedule = require('node-schedule');
 // 可以按照 cron 的格式設定
 function runSchedule (cb) {
   // cron 風格的配置：每 5 分鐘執行一次
-  schedule.scheduleJob('0 17 * * * *', function () {
+  schedule.scheduleJob('0 17 */3 * * *', function () {
     // console.log('定時任務執行一次');
     console.log("scheduleCronstyle:" + new Date());
     cb && cb();
